911e8763101e8da5ea7c4570113ee99953ae75d0
kaiodeodato <kaiod.oficial@gmail.com>
2023-03-15 14:49:52 +0000

    start layout

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // console.log(myData)
 // console.log(myData.results)
 
-const data = async ()=>{
-    const results = await fetch('https://kaiodeodato.github.io/API_AI_STORE/myData.json')
-    const parseData = await results.json()
-    console.log(parseData)
-}
-data()
+// const data = async ()=>{
+//     const results = await fetch('https://kaiodeodato.github.io/API_AI_STORE/myData.json')
+//     const parseData = await results.json()
+//     console.log(parseData)
+// }
+// data()
