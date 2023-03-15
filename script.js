473fa5eb3a143b7dc311347b28d594b60e10fb67
kaiodeodato <kaiod.oficial@gmail.com>
2023-03-15 14:01:53 +0000

    teste pasta

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-import myData from './myData.json' assert {type: 'json'};
+// import myData from './myData.json' assert {type: 'json'};
 
-console.log(myData)
-console.log(myData.results)
+// console.log(myData)
+// console.log(myData.results)
+
+const data = async ()=>{
+    const results = await fetch('https://kaiodeodato.github.io/API_AI_STORE/myData.json')
+    const parseData = await results.json()
+    console.log(parseData)
+}
+data()
