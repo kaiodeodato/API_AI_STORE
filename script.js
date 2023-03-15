829b142bebd79dd02c800db7a70b14556d80ea6e
kaiodeodato <kaiod.oficial@gmail.com>
2023-03-15 18:19:17 +0000

    add new files and config copy icon

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// import myData from './myData.json' assert {type: 'json'};
 
-// console.log(myData)
-// console.log(myData.results)
-
-// const data = async ()=>{
-//     const results = await fetch('https://kaiodeodato.github.io/API_AI_STORE/myData.json')
-//     const parseData = await results.json()
-//     console.log(parseData)
-// }
-// data()
+function copyEvent(id)
+{
+    var str = document.getElementById(id);
+    window.getSelection().selectAllChildren(str);
+    document.execCommand("Copy")
+    alert('copiei')
+}
