997f05555b25bf085977222754c0381cf0db0d23
kaiodeodato <kaiod.oficial@gmail.com>
2023-03-16 18:13:22 +0000

    change url test api

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 
+
+async function fetchData() {
+
+    const response = await fetch('https://kaiodeodato.github.io/API_AI_STORE/myData.json');
+    const products = await response.json();
+    console.log(products)
+    return products;
+    }
+fetchData()
+
+
 function copyEvent(id)
 {
     var str = document.getElementById(id);
