b37c97d8dd58d8777a44cca1aa38202b4de1ceba
kaiodeodato <kaiod.oficial@gmail.com>
2023-03-17 18:10:47 +0000

    update api shoes

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 
 async function fetchData() {
 
-    const response = await fetch('https://kaiodeodato.github.io/API_AI_STORE/myData.json');
-    const products = await response.json();
+    const response = await fetch('https://kaiodeodato.github.io/API_AI_STORE/data/myDataShoes.json');
+    const productsData = await response.json();
+    // console.log(productsData)
+
+
+    //teste API produtos
+    const products = productsData.results
     console.log(products)
+    const ArrayImagens = products.map(item => {
+        return (`
+            <img class="imgTeste" src="${item.image}">
+        `)
+    })
+    const ElementosImages = document.getElementById('teste')
+    ElementosImages.innerHTML = ArrayImagens.join("")
+
+
     return products;
     }
 fetchData()
 
 
 
-
+//copy event
 function copyEvent(id)
 {
     var str = document.getElementById(id);
